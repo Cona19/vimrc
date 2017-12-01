d9707bec7a68234791caf55b9d83667c45249ac5
Hyeonseok Oh <hyeonseok.ohh@gmail.com>
2017-12-01 12:57:30 +0900

    add mkcscope.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@
 cp .vimrc ~/
 mkdir -p ~/.vim/plugin
 cp cscope_maps.vim ~/.vim/plugin
+mkdir -p ~/.bin
+cp mkcscope.sh ~/.bin
+echo "export PATH=~/.bin/:\$PATH" >> ~/.bashrc
---
+++ mkcscope.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+rm -rf cscope.files cscope.files
+#find . \( -name '*.c' -o -name '*.i' -o -name '*.ic' -o -name '*.cpp' \
+#    -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \
+#    -o -name '*.hpp' \) -print | grep -v example | grep -v test > cscope.files
+
+find . \( -name '*.c' -o -name '*.i' -o -name '*.ic' -o -name '*.cpp' \
+    -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \
+    -o -name '*.hpp' \) -print > cscope.files
+
+cscope -i cscope.files
